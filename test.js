86b9063017b59f8ae3ac065c1b798efa97f195fc
Mohammad Shahnawaz Alam <mshahnawaz9935@gmail.com>
2017-03-09 17:04:27 +0000

    update

--- test.js
+++ test.js
@@ -9,16 +9,21 @@ var parser = parse({delimiter: ','}, function (err, data) {
 
   async.eachSeries(data, function (line, callback) {
   //  console.log(line);
-    latitude.push(line[0]);
+    latitude.push(line[0]);longitude.push(line[1]);timestamp.push(line[2]);
     callback();
     x++;
   })
-  storedata(latitude);
+  storedata(latitude,longitude,timestamp);
 });
 fs.createReadStream(inputFile).pipe(parser);
-function storedata(data)
+function storedata(data1,data2,data3)
 {
-    console.log(data);
-
+    var length = data1.length;
+    for(var i=0;i< length;i++)
+    {
+      if(i==length-1)
+      continue;
+       var time = data3[i+1] - data3[i];
+       console.log('time', time );
+    }
 }
-
